d9670a34d32aeff0defe7a2b7bab0903697a83ed
Kunanon <fronkzoza001@gmail.com>
2021-06-10 16:40:40 +0700

    2021 First Updated

---
+++ android/app/src/main/kotlin/com/example/theurban/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.theurban
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/services/functions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-void changeScreen(BuildContext context, Widget widget){
-  Navigator.push(context, MaterialPageRoute(builder: (_) => widget));
-}
-
-void changeScreenReplacement(BuildContext context, Widget widget){
-  Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => widget));
-}
